8559fc0346c008afb3ae6526373cfacf96afa0a2
visweswarji <visweswar5555955@gmail.com>
2023-07-13 08:46:56 +0530

    commiting

---
+++ app2/player.ts
@@ -0,0 +1,9 @@
+import { Person } from './person';
+
+export class Player implements Person {
+    name: string;
+    highScore: number;
+    formatName() {
+        return this.name.toUpperCase();
+    }
+}
