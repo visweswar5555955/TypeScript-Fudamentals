fc2da6785ec69057fb63701ec6e55d1e50ebcfed
visweswarji <visweswar5555955@gmail.com>
2023-07-13 08:40:58 +0530

    commiting back

--- app/player.ts
+++ app/player.ts
@@ -3,7 +3,6 @@ import { Person } from './person';
 export class Player implements Person {
     name: string;
     highScore: number;
-    sai : string
     formatName() {
         return this.name.toUpperCase();
     }
